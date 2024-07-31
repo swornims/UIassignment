2833e8eb1e70c78acecc22c7c3e2fe8cd2e7aff1
swornims <iamswornimshakya@gmail.com>
2024-07-31 19:04:42 +0545

    Added a map folder

---
+++ map/map.ts
@@ -0,0 +1,129 @@
+import author1 from "@/public/pfp.png";
+import author2 from "@/public/author2.png";
+import author3 from "@/public/author3.png";
+
+export const features = [
+  {
+    heading: "Dashboard",
+    content:
+      "See a quick snapshot of your community with total members, of which are actively engaged. Recent activity has been hot with new sign-ups and check-ins..",
+  },
+  {
+    heading: "Membership Management",
+    content:
+      "Manage your members effortlessly with a searchable member list. Dive deeper with detailed member profiles, and easily welcome new members through a simple form.",
+  },
+  {
+    heading: "Check-ins",
+    content:
+      "Track attendance seamlessly through a Check-In List showcasing recent check-ins with timestamps.",
+  },
+  {
+    heading: "Bills and Payments",
+    content:
+      "Manage invoices with ease, view a complete Invoices List with statuses (paid, pending, overdue).",
+  },
+  {
+    heading: "Reporting",
+    content:
+      "Enhance security and manage access control effortlessly. Monitor entry and exit activity with detailed Access Logs, and maintain vigilance with Real-Time Monitoring features.",
+  },
+  {
+    heading: "Settings",
+    content:
+      "Manage everything in one place with our streamlined settings. Handle user accounts, configure membership plans, and get quick customer support.",
+  },
+];
+
+export const faq = [
+  {
+    question: "Can I import my existing member data?",
+    answer:
+      "Absolutely, our software supports easy import of your existing member data in excel or csv formats",
+  },
+  {
+    question: "How secure is my data?",
+    answer:
+      "Absolutely, our software supports easy import of your existing member data in excel or csv formats",
+  },
+  {
+    question: "Can multiple users access the software simultaneously?",
+    answer:
+      "Absolutely, our software supports easy import of your existing member data in excel or csv formats",
+  },
+];
+
+export const pricingPlans = [
+  {
+    plan: "Basic",
+    price: "$80/mth",
+    features: [
+      "Upto 100 members",
+      "48hr support ticket resolve",
+      "80/month",
+      "Unlimited Staff",
+      "Free Training (Online)",
+      "Custom website",
+      "Track your reputation (Available as additional add-on)",
+    ],
+    bgColor: "bg-white",
+    textColor: "text-black",
+    buttonBgColor: "bg-slate-50",
+  },
+  {
+    plan: "Standard",
+    price: "$1500/mth",
+    features: [
+      "100-300 members",
+      "24hr support ticket resolve",
+      "1500/month",
+      "Unlimited Staff",
+      "Free Training (Online)",
+      "Custom website",
+      "Track your reputation (Available as additional add-on)",
+    ],
+    bgColor: "bg-[#3949AB]",
+    textColor: "text-white",
+    buttonBgColor: "bg-transparent",
+  },
+  {
+    plan: "Platinum",
+    price: "$80/mth",
+    features: [
+      "Unlimited members",
+      "12hr support ticket resolve",
+      "Contact sales",
+      "Unlimited Staff",
+      "Free Training (Online/ On-site)",
+      "Custom website",
+      "Track your reputation (Available as additional add-on)",
+    ],
+    bgColor: "bg-white",
+    textColor: "text-black",
+    buttonBgColor: "bg-slate-50",
+  },
+];
+
+export const testimonials = [
+  {
+    author: "Jenny Wilson",
+    authorImg: author1,
+    title: "Energym",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu etdictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.",
+  },
+  {
+    author: "Andrew Wilson",
+    authorImg: author2,
+    title: "Energym",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu etdictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.",
+  },
+  {
+    author: "Harry Wilson",
+    authorImg: author3,
+    title: "Energym",
+    description:
+      "Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu etdictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.Lorem ipsum dolor sit amet consectetur. Sit risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.risus et proin mi eu et dictumst. Sem feugiat lectus sed rhoncus nisi ut mi.",
+  },
+];
